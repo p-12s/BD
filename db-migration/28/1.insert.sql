97038e57e8e3f169bb9eeaf35b53db1d65c77710
Pavel V <mental.notes@yandex.ru>
2018-02-26 23:48:20 +0300

    Доработал запросы по заданию 3.4

--- db-migration/28/1.insert.sql
+++ db-migration/28/1.insert.sql
@@ -10,4 +10,11 @@ INSERT INTO [goods_sellers_sales].[dbo].[Product] ([Name], [Manufacture], [Price
   VALUES 
     (N'Апельсины', N'ООО "Апельсин"', 10.50)
 
--- 3. С чтением значения из другой таблицы         ?
+-- 3. С чтением значения из другой таблицы
+
+INSERT INTO [goods_sellers_sales].[dbo].[Product] ([Name], [Manufacture], [Price])
+SELECT (Name + 'ка'), ('ООО ' + Surname), (SellerId * 100)
+FROM [goods_sellers_sales].[dbo].[Seller]
+
+SELECT * FROM [goods_sellers_sales].[dbo].[Product]
+
--- db-migration/28/4.select.sql
+++ db-migration/28/4.select.sql
@@ -13,3 +13,86 @@ FROM [goods_sellers_sales].[dbo].[Product]
 SELECT * 
 FROM [goods_sellers_sales].[dbo].[Product]
 WHERE Name = 'Слива'
+
+-- 4.a - ASC
+SELECT *
+FROM [goods_sellers_sales].[dbo].[Product]
+ORDER BY Name
+ASC
+
+-- 4.b - DESC
+SELECT *
+FROM [goods_sellers_sales].[dbo].[Product]
+ORDER BY Name
+DESC
+
+-- 4.c - по двум атрибутам
+SELECT *
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+ORDER BY ProductId, Count
+ASC
+
+-- 4.d - по первому атрибуту, из списка извлекаемых
+SELECT *
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+ORDER BY 1
+ASC
+
+-- 2. SELECT с функциями агрегации
+
+-- a. 4.1 MIN
+-- b. 4.2 MAX 
+-- c. 4.3 AVG
+-- d. 4.4 SUM
+SELECT * FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+SELECT 
+  SellingId, 
+  MIN(Count) AS MinCountForSellingId, 
+  MAX(Count) AS MaxCountForSellingId,
+  AVG(Count) AS AvgCountForSellingId,
+  Sum(Count) AS SumCountForSellingId
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+GROUP BY SellingId    
+
+
+-- 3. GROUP BY + текстовое объяснение извлекаемых данных
+-- a. функция агрегации GROUP BY
+SELECT * FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+
+SELECT 
+  SellingId, 
+  COUNT(ProductId) AS ProductCountInSelling
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+GROUP BY SellingId 
+
+-- b. функция агрегации GROUP BY HAVING                              
+SELECT * FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+
+SELECT 
+  SellingId, 
+  Sum(ProductID) AS Sum
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+GROUP BY SellingId
+
+SELECT 
+  SellingId, 
+  Sum(ProductID) AS Sum
+FROM [goods_sellers_sales].[dbo].[ProductInSelling]
+GROUP BY SellingId
+HAVING Sum(ProductId) BETWEEN 1.0 AND 3.0
+ORDER BY SellingId
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
