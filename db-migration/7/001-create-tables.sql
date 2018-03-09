862e4a520dce3a4c751eb55a4b82669357b6ab30
Pavel V <mental.notes@yandex.ru>
2018-03-09 20:50:25 +0300

    Убрал лишние [] в названиях таблиц и столбцов

--- db-migration/7/001-create-tables.sql
+++ db-migration/7/001-create-tables.sql
@@ -1,12 +1,12 @@
 -------------- create table Company --------------
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Company' AND xtype='U')
-  CREATE TABLE [Company](
-    [CompanyId] [int] IDENTITY(1, 1) NOT NULL,
-    [Name] [nvarchar](100) NOT NULL,
-    [YearOfFoundation] [int] NOT NULL,
-    CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED 
+  CREATE TABLE Company (
+    CompanyId int IDENTITY(1, 1) NOT NULL,
+    Name nvarchar (100) NOT NULL,
+    YearOfFoundation int NOT NULL,
+    CONSTRAINT PK_Company PRIMARY KEY CLUSTERED 
     (
-      [CompanyId] ASC
+      CompanyId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
@@ -16,13 +16,13 @@ GO
 
 -------------- create table Dealer --------------
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Dealer' AND xtype='U')
-  CREATE TABLE [Dealer](
-    [DealerId] [int] IDENTITY(1, 1) NOT NULL,
-    [Surname] [nvarchar](100) NOT NULL,
-    [Phone] [nvarchar](100) NOT NULL,
-    CONSTRAINT [PK_dbo.Dealer] PRIMARY KEY CLUSTERED 
+  CREATE TABLE Dealer (
+    DealerId int IDENTITY(1, 1) NOT NULL,
+    Surname nvarchar (100) NOT NULL,
+    Phone nvarchar (100) NOT NULL,
+    CONSTRAINT PK_Dealer PRIMARY KEY CLUSTERED 
     (
-      [DealerId] ASC
+      DealerId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
@@ -32,13 +32,13 @@ GO
 
 -------------- create table Medicine --------------
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Medicine' AND xtype='U')
-  CREATE TABLE [Medicine](
-    [MedicineId] [int] IDENTITY(1, 1) NOT NULL,
-    [Name] [nvarchar](100) NOT NULL,
-    [DurationOfTreatment] [int] NOT NULL,
-    CONSTRAINT [PK_dbo.Medicine] PRIMARY KEY CLUSTERED 
+  CREATE TABLE Medicine (
+    MedicineId int IDENTITY(1, 1) NOT NULL,
+    Name nvarchar (100) NOT NULL,
+    DurationOfTreatment int NOT NULL,
+    CONSTRAINT PK_Medicine PRIMARY KEY CLUSTERED 
     (
-      [MedicineId] ASC
+      MedicineId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
@@ -48,16 +48,16 @@ GO
 
 -------------- create table Production --------------
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Production' AND xtype='U')
-  CREATE TABLE [Production](
-    [ProductionId] [int] IDENTITY(1, 1) NOT NULL,
-    [CompanyId] [int] NOT NULL,
-    [MedicineId] [int] NOT NULL,
-    [Price] [money] NOT NULL,
-    [QualityControl] [int] NOT NULL,
-    [DealerId] [int] NOT NULL,
-    CONSTRAINT [PK_dbo.Production] PRIMARY KEY CLUSTERED 
+  CREATE TABLE Production (
+    ProductionId int IDENTITY(1, 1) NOT NULL,
+    CompanyId int NOT NULL,
+    MedicineId int NOT NULL,
+    Price money NOT NULL,
+    QualityControl int NOT NULL,
+    DealerId int NOT NULL,
+    CONSTRAINT PK_Production PRIMARY KEY CLUSTERED 
     (
-      [ProductionId] ASC
+      ProductionId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
@@ -102,7 +102,7 @@ IF OBJECT_ID('FK_DealerProduction') IS NULL
   GO
 -------------- create table Drugstore --------------
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Drugstore' AND xtype='U')
-  CREATE TABLE [Drugstore](
+  CREATE TABLE Drugstore (
     DrugstoreId int IDENTITY(1, 1) NOT NULL,
     Name nvarchar (100) NOT NULL,
     Address nvarchar (255) NOT NULL,
