8e9e5e30b605a8a7654d61d28cdfbd6598d5ca2c
Pavel V <mental.notes@yandex.ru>
2018-03-11 18:34:19 +0300

    Добавил запрос для "Дать минимальный и максимальный баллы по ..."

--- db-migration/7/requests.sql
+++ db-migration/7/requests.sql
@@ -28,9 +28,44 @@ INNER JOIN Drugstore
 WHERE (Company.Name LIKE '%Аргус%') AND (Medicine.Name LIKE '%Кордерон%')
 
 -- 3) Дать список лекарств компании “Фарма”, на которые не были сделаны заказы до 1.05.12.
+SELECT 
+  *
+FROM Production
+LEFT JOIN [Order]
+  ON Production.ProductionId = [Order].ProductionId
+INNER JOIN Medicine 
+  ON Production.MedicineId = Medicine.MedicineId
+INNER JOIN Company
+  ON Production.CompanyId = Company.CompanyId
+WHERE (Company.Name LIKE '%Фарма%') AND ([Order].OrderDate < '2012-05-01') AND ([Order].ProductionId IS NULL)
+
+
+SELECT 
+  *
+FROM Production
+LEFT JOIN [Order]
+  ON Production.ProductionId = [Order].ProductionId
+WHERE ([Order].ProductionId IS NULL)
 
 -- 4) Дать минимальный и максимальный баллы по лекарствам каждой фирмы, которая производит не менее 10 препаратов, с указанием названий фирмы и лекарства.
+
+SELECT
+  ProductionId, CompanyId, COUNT(MedicineId) AS 'Medicine count'
+FROM Production
+GROUP BY ProductionId
+
+
+SELECT 
+  SellingId, 
+  Sum(ProductID) AS Sum
+FROM ProductInSelling
+GROUP BY SellingId
+HAVING Sum(ProductId) BETWEEN 1.0 AND 3.0
+ORDER BY SellingId
 
 -- 5) Дать списки сделавших заказы аптек по всем дилерам компании “Гедеон Рихтер”. Если у дилера нет заказов, в названии аптеки проставить NULL.
 
 -- 6) Уменьшить на 20% стоимость всех лекарств, если она превышает 3000, а длительность лечения не более 7 дней.
+
+
+
