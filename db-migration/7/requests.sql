554b552bcd9659a97d6c76d5468b62c7399bc901
Pavel V <mental.notes@yandex.ru>
2018-03-26 12:14:40 +0300

    Добавил запрос

--- db-migration/7/requests.sql
+++ db-migration/7/requests.sql
@@ -9,46 +9,18 @@ INNER JOIN sys.foreign_key_columns AS fc
    ON f.OBJECT_ID = fc.constraint_object_id
 
 -- 2) Выдать информацию по всем заказам лекарства “Кордерон” компании “Аргус” с указанием названий аптек, дат, объема заказов.
-SELECT 
-  Medicine.Name AS 'Drug', 
-  Company.Name AS 'Company name',
-  Drugstore.Name AS 'Drugstore name',
-  Drugstore.Address AS 'Drugstore address',
-  [Order].OrderDate AS 'Date ordering',
-  [Order].QuantityMedicineInOrder AS 'Order quantity'
-FROM Production
-INNER JOIN Medicine 
-  ON Production.MedicineId = Medicine.MedicineId
-INNER JOIN Company
-  ON Production.CompanyId = Company.CompanyId
-INNER JOIN [Order]
-  ON Production.ProductionId = [Order].ProductionId
-INNER JOIN Drugstore
-  ON [Order].DrugstoreId = Drugstore.DrugstoreId
-WHERE (Company.Name LIKE '%Аргус%') AND (Medicine.Name LIKE '%Кордерон%')
+SELECT D.Name, O.OrderDate, O.QuantityMedicineInOrder FROM Orders AS O
+LEFT JOIN Drugstore AS D ON O.DrugstoreId = D.DrugstoreId
+WHERE O.ProductionId = (SELECT P.ProductionId FROM Production AS P 
+WHERE CompanyId = (SELECT C.CompanyId FROM Company AS C WHERE C.Name LIKE '%Аргус%') AND MedicineId = (SELECT M.MedicineId FROM Medicine AS M WHERE M.Name LIKE '%Кордерон%'))
 
 -- 3) Дать список лекарств компании “Фарма”, на которые не были сделаны заказы до 1.05.12.
-SELECT 
-  *
-FROM Production
-LEFT JOIN [Order]
-  ON Production.ProductionId = [Order].ProductionId
-INNER JOIN Medicine 
-  ON Production.MedicineId = Medicine.MedicineId
-INNER JOIN Company
-  ON Production.CompanyId = Company.CompanyId
-WHERE (Company.Name LIKE '%Фарма%') AND ([Order].OrderDate < '2012-05-01') AND ([Order].ProductionId IS NULL)
-
-
-SELECT 
-  *
-FROM Production
-LEFT JOIN [Order]
-  ON Production.ProductionId = [Order].ProductionId
-WHERE ([Order].ProductionId IS NULL)
+SELECT M.Name FROM Medicine AS M WHERE M.MedicineId IN (
+SELECT P.MedicineId FROM Production AS P 
+LEFT JOIN Orders AS O ON P.ProductionId = O.OrderId 
+WHERE CompanyId = (SELECT C.CompanyId FROM Company AS C WHERE C.Name LIKE '%Фарма%') AND (O.OrderDate IS NULL OR O.OrderDate > '2012-05-01'))
 
 -- 4) Дать минимальный и максимальный баллы по лекарствам каждой фирмы, которая производит не менее 10 препаратов, с указанием названий фирмы и лекарства
-
 SELECT P.CompanyId, C.Name, MIN(P.QualityControl) AS MinQ, MAX(P.QualityControl) AS MaxQ
 FROM Production AS P
 LEFT JOIN Company AS C ON P.CompanyId = C.CompanyId
@@ -56,11 +28,11 @@ GROUP BY P.CompanyId, C.Name
 HAVING COUNT(*) > 10
 
 -- 5) Дать списки сделавших заказы аптек по всем дилерам компании “Годеон Рихтер”. Если у дилера нет заказов, в названии аптеки проставить NULL.
-                                                         
 SELECT PP.DealerId, O.DrugstoreId, D.Name FROM 
-(SELECT P.ProductionId, P.DealerId FROM Production AS P WHERE CompanyId = (SELECT C.CompanyId FROM Company AS C WHERE C.Name LIKE '%������ ������%')) AS PP
+(SELECT P.ProductionId, P.DealerId FROM Production AS P WHERE CompanyId = (SELECT C.CompanyId FROM Company AS C WHERE C.Name LIKE '%Годеон Рихтер%')) AS PP
 LEFT JOIN Orders AS O ON PP.productionId = O.ProductionId
 INNER JOIN Drugstore AS D ON O.DrugstoreId = D.DrugstoreId
+GROUP BY PP.DealerId, O.DrugstoreId, D.Name
 
 -- 6) Уменьшить на 20% стоимость всех лекарств, если она превышает 3000, а длительность лечения не более 7 дней.
 UPDATE
