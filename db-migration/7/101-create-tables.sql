2ce2a6180160c6b1f3b4567f341b8defdac0a9c6
Pavel V <mental.notes@yandex.ru>
2018-03-26 18:01:10 +0300

    Добавил создание таблиц

--- db-migration/7/101-create-tables.sql
+++ db-migration/7/101-create-tables.sql
@@ -55,130 +55,59 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Room' AND xtype='U')
   ON [PRIMARY]
 GO
 
-
-4)	Клиенты: client - id клиента, фио, телефон.
-5)  Бронь: id - брони, id клиента, дата бронирования
-6)  Комната в брони: id, id брони, id категории номера, дата заезда, дата выезда.
-
-
-
-
-
--------------- create table Medicine --------------
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Medicine' AND xtype='U')
-  CREATE TABLE Medicine (
-    MedicineId int IDENTITY(1, 1) NOT NULL,
-    Name nvarchar (100) NOT NULL,
-    DurationOfTreatment int NOT NULL,
-    CONSTRAINT PK_Medicine PRIMARY KEY CLUSTERED 
+-------------- create table Client --------------
+-- 4)	Клиенты: client - id клиента, фио, телефон.
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Client' AND xtype='U')
+  CREATE TABLE Client (
+    ClientId int IDENTITY(1, 1) NOT NULL,
+    FullName nvarchar (255) NOT NULL,
+    Phone nvarchar (100) NOT NULL,
+    CONSTRAINT PK_Client PRIMARY KEY CLUSTERED 
     (
-      MedicineId ASC
+      ClientId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
   ) 
   ON [PRIMARY]
-GO          
+GO 
 
--------------- create table Production --------------
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Production' AND xtype='U')
-  CREATE TABLE Production (
-    ProductionId int IDENTITY(1, 1) NOT NULL,
-    CompanyId int NOT NULL,
-    MedicineId int NOT NULL,
-    Price money NOT NULL,
-    QualityControl int NOT NULL,
-    DealerId int NOT NULL,
-    CONSTRAINT PK_Production PRIMARY KEY CLUSTERED 
+-------------- create table Reservation --------------
+-- 5)  Бронь: id - брони, id клиента, дата бронирования
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Reservation' AND xtype='U')
+  CREATE TABLE Reservation (
+    ReservationId int IDENTITY(1, 1) NOT NULL,
+    ClientId int NOT NULL,
+	ReservationDate date NOT NULL,
+    CONSTRAINT PK_Reservation PRIMARY KEY CLUSTERED 
     (
-      ProductionId ASC
+      ReservationId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
-  )
+  ) 
   ON [PRIMARY]
-GO
+GO 
 
-IF OBJECT_ID('FK_MedicineProduction') IS NULL
-  ALTER TABLE [Production]
-    WITH CHECK
-    ADD CONSTRAINT FK_MedicineProduction
-    FOREIGN KEY (MedicineId) REFERENCES Medicine(MedicineId)
-    ON DELETE CASCADE
-  GO
-
-  ALTER TABLE [Production] 
-    CHECK CONSTRAINT FK_MedicineProduction
-  GO  
-
-IF OBJECT_ID('FK_CompanyProduction') IS NULL
-  ALTER TABLE [Production]
-    WITH CHECK
-    ADD CONSTRAINT FK_CompanyProduction
-    FOREIGN KEY (CompanyId) REFERENCES Company(CompanyId)
-    ON DELETE CASCADE
-  GO
-
-  ALTER TABLE [Production] 
-    CHECK CONSTRAINT FK_CompanyProduction
-  GO
-
-IF OBJECT_ID('FK_DealerProduction') IS NULL
-  ALTER TABLE [Production]
-    WITH CHECK
-    ADD CONSTRAINT FK_DealerProduction
-    FOREIGN KEY (DealerId) REFERENCES Dealer(DealerId)
-    ON DELETE CASCADE
-  GO
-
-  ALTER TABLE [Production] 
-    CHECK CONSTRAINT FK_DealerProduction
-  GO
--------------- create table Drugstore --------------
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Drugstore' AND xtype='U')
-  CREATE TABLE Drugstore (
-    DrugstoreId int IDENTITY(1, 1) NOT NULL,
-    Name nvarchar (100) NOT NULL,
-    Address nvarchar (255) NOT NULL,
-    CONSTRAINT PK_Drugstore PRIMARY KEY CLUSTERED 
+-------------- create table ArmorRoom --------------
+-- 6)  Комната в брони: id, id брони, id категории номера, дата заезда, дата выезда.
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ArmorRoom' AND xtype='U')
+  CREATE TABLE ArmorRoom (
+    ArmorRoomId int IDENTITY(1, 1) NOT NULL,
+    ReservationId int NOT NULL,
+	CategoryId int NOT NULL,
+	ArrivalDate date NOT NULL,
+	DepartureDate date NOT NULL,
+    CONSTRAINT PK_ArmorRoom PRIMARY KEY CLUSTERED 
     (
-      DrugstoreId ASC
+      ArmorRoomId ASC
     )
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
     ON [PRIMARY]
-  )
+  ) 
   ON [PRIMARY]
-GO
+GO 
 
--------------- create table Order --------------
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Order' AND xtype='U')
-  CREATE TABLE [Order](
-    OrderId int IDENTITY(1, 1) NOT NULL,
-    DrugstoreId int NOT NULL,
-    ProductionId int NOT NULL,
-    OrderDate date NOT NULL,     
-    QuantityMedicineInOrder int NOT NULL,
-    CONSTRAINT PK_Order PRIMARY KEY CLUSTERED 
-    (
-      OrderId ASC
-    )
-    WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) 
-    ON [PRIMARY]
-  )
-  ON [PRIMARY]
-GO
-
-IF OBJECT_ID('FK_DrugstoreOrder') IS NULL
-  ALTER TABLE [Order]
-    WITH CHECK
-    ADD CONSTRAINT FK_DrugstoreOrder
-    FOREIGN KEY (DrugstoreId) REFERENCES Drugstore(DrugstoreId)
-    ON DELETE CASCADE
-  GO
-
-  ALTER TABLE [Order] 
-    CHECK CONSTRAINT FK_DrugstoreOrder
-  GO  
 
 IF OBJECT_ID('FK_ProductionOrder') IS NULL
   ALTER TABLE [Order]
