d28536e7650a425c6c92a63a78bb68e1215c5f12
Pavel V <mental.notes@yandex.ru>
2018-04-21 15:03:52 +0300

    Отмечать "+" правильные индексы было плохой идеей
    Приходилось их убирать при вставке запроса в MSMS

--- db-migration/3. Booking/5-indexes.sql
+++ db-migration/3. Booking/5-indexes.sql
@@ -13,37 +13,31 @@ ORDER BY Cl.ClientId ASC
 
 -- from 0.126568 to 0.0463841 (~60%)
 -- composite Room = 0.0627325
--- composite Room + unique Name in Category = 0.124514
--- composite Room + unique Name in Category + Name in Hotel = 0.0642507
--- unique Name in Category + unique Name in Hotel = 0.110771
+-- composite Room - unique Name in Category = 0.124514
+-- composite Room - unique Name in Category - Name in Hotel = 0.0642507
+-- unique Name in Category - unique Name in Hotel = 0.110771
 
-+-- make Name in Category table as unique index
+--- make Name in Category table as unique index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'UI_Category_Name')
 	DROP INDEX UI_Category_Name ON Category
 	CREATE UNIQUE INDEX UI_Category_Name ON Category (Name)
 GO
-+-- make Name in Hotel table as index
+--- make Name in Hotel table as index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Hotel_Name')
 	DROP INDEX [IX_Hotel_Name] ON Hotel
 	CREATE INDEX [IX_Hotel_Name] ON Hotel (Name)
 GO
-+-- make HotelId-CategoryId in Room table as composite index 
+--- make HotelId-CategoryId in Room table as composite index 
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Room_HotelId-CategoryId')
 	DROP INDEX [IX_Room_HotelId-CategoryId] ON Room
 	CREATE INDEX [IX_Room_HotelId-CategoryId] ON Room (HotelId, CategoryId)
 GO
-+-- make RoomId-ReservationId in RoomInReservation as composite index
+--- make RoomId-ReservationId in RoomInReservation as composite index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_RoomId-ReservationId')
 	DROP INDEX [IX_RoomInReservation_RoomId-ReservationId] ON RoomInReservation
 	CREATE INDEX [IX_RoomInReservation_RoomId-ReservationId] ON RoomInReservation (RoomId, ReservationId)
 GO
-
-+-- make ReservationId-ClientId in Reservation as composite index
-IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Reservation_ReservationId-ClientId')
-	DROP INDEX [IX_Reservation_ReservationId-ClientId] ON Reservation
-	CREATE INDEX [IX_Reservation_ReservationId-ClientId] ON Reservation (ReservationId, ClientId)
-GO
-+-- make ClientId in Reservation as index - a little bit faster
+--- make ClientId in Reservation as index - a little bit faster
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Reservation_ClientId')
 	DROP INDEX [IX_Reservation_ClientId] ON Reservation
 	CREATE INDEX [IX_Reservation_ClientId] ON Reservation (ClientId)
@@ -57,22 +51,24 @@ SELECT * FROM Room WHERE RoomId NOT IN (
   WHERE '2012-05-30' BETWEEN RiR.ArrivalDate AND RiR.DepartureDate
 )
 ORDER BY RoomId, HotelId
+-- SELECT * FROM RoomInReservation WHERE RoomId = 105
 
-+-- from 0.0310008 to 0.0305198 (~2%)
+
+--- from 0.0310008 to 0.0305198 (~2%)
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_ArrivalDate-DepartureDate')
 	DROP INDEX [IX_RoomInReservation_ArrivalDate-DepartureDate] ON RoomInReservation
 	CREATE INDEX [IX_RoomInReservation_ArrivalDate-DepartureDate] ON RoomInReservation (ArrivalDate, DepartureDate)
 GO
-+-- make separately index - a little bit faster
+--- make separately index - a little bit faster
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_ArrivalDate')
 	DROP INDEX [IX_RoomInReservation_ArrivalDate] ON RoomInReservation
 	CREATE INDEX [IX_RoomInReservation_ArrivalDate] ON RoomInReservation (ArrivalDate)
 GO
+
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_DepartureDate')
 	DROP INDEX [IX_RoomInReservation_DepartureDate] ON RoomInReservation
 	CREATE INDEX [IX_RoomInReservation_DepartureDate] ON RoomInReservation (DepartureDate)
 GO
-
 -- make RoomId in RoomInReservation as index - no result (although it should be)
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_RoomId')
 	DROP INDEX [IX_RoomInReservation_RoomId] ON RoomInReservation
@@ -91,7 +87,7 @@ WHERE '2012-05-25' BETWEEN RiR.ArrivalDate AND RiR.DepartureDate AND H.Name = 'Ð
 GROUP BY C.CategoryId
 
 -- from 0.0366261 to 0.0317572 (~15%)
-+-- make separately index - a little bit faster. why? scan whatever!
+--- make separately index - a little bit faster. why? scan whatever!
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_RoomInReservation_ArrivalDate')
 	DROP INDEX [IX_RoomInReservation_ArrivalDate] ON RoomInReservation
 	CREATE INDEX [IX_RoomInReservation_ArrivalDate] ON RoomInReservation (ArrivalDate)
@@ -105,7 +101,7 @@ IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Hotel_Name')
 	DROP INDEX [IX_Hotel_Name] ON Hotel
 	CREATE INDEX [IX_Hotel_Name] ON Hotel (Name)
 GO
-+--- make separately index - a little bit faster
+---- make separately index - a little bit faster
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Room_CategoryId')
 	DROP INDEX [IX_Room_CategoryId] ON Room
 	CREATE INDEX [IX_Room_CategoryId] ON Room (CategoryId)
@@ -232,17 +228,17 @@ BEGIN TRANSACTION
   VALUES (@reservationId, @roomId, @arrivalDate, @departureDate)
 COMMIT TRANSACTION
 
-+-- make FullName in Client as index
+--- make FullName in Client as index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Client_FullName')
 	DROP INDEX [IX_Client_FullName] ON Client
 	CREATE INDEX [IX_Client_FullName] ON Client (FullName)
 GO
-+-- make Name in Hotel as index
+--- make Name in Hotel as index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'IX_Hotel_Name')
 	DROP INDEX [IX_Hotel_Name] ON Hotel
 	CREATE INDEX [IX_Hotel_Name] ON Hotel (Name)
 GO
-+-- make Name in Category as index
+--- make Name in Category as index
 IF EXISTS (SELECT name from sys.indexes WHERE name = N'UI_Category_Name')
 	DROP INDEX [UI_Category_Name] ON Category
 	CREATE UNIQUE INDEX [UI_Category_Name] ON Category (Name)
