ce3a56a9d8946bfdf65c0c9a7edfdddf1d4d1d0f
Pavel V <mental.notes@yandex.ru>
2018-04-23 23:03:19 +0300

    Добавил запросы в бд Education

--- db-migration/4. Education/4-requests.sql
+++ db-migration/4. Education/4-requests.sql
@@ -15,27 +15,49 @@ SELECT * FROM mathematic_ratings
 -- 3)	Дать информацию о должниках с указанием фамилии студента и названия предмета. 
 --		Должниками считаются студенты, не имеющие оценки по предмету, который ведется в группе. 
 --		Оформить в виде процедуры, на вход название группы.
-
 CREATE PROC Get_debtors_of_group @GroupName varchar(60)
 AS    
-  SELECT C.Abbreviation, S.Surname, Sub.Name FROM Class AS C
-  LEFT JOIN Student AS S ON S.ClassId = C.ClassId
-  LEFT JOIN Job AS J ON J.ClassId = C.ClassId
-  LEFT JOIN Subject AS Sub ON Sub.SubjectId = J.SubjectId
-  LEFT JOIN Rating AS R ON R.JobId = J.JobId
-  WHERE C.Abbreviation = @GroupName AND R.Rating IS NULL
-  ORDER BY S.Surname, Sub.Name;
+  SELECT S.Surname, Sub.Name FROM Student AS S
+  INNER JOIN Class AS C ON C.ClassId = S.ClassId
+  INNER JOIN Job AS J ON J.ClassId = C.ClassId
+  LEFT JOIN Rating AS R ON R.StudentId = S.StudentId AND R.JobId = J.JobId
+  INNER JOIN Subject AS Sub ON Sub.SubjectId = J.SubjectId
+  WHERE C.Abbreviation = @GroupName
+  GROUP BY S.Surname, Sub.Name
+  HAVING COUNT(R.Rating) = 0
+  ORDER BY S.Surname
 
 EXEC Get_debtors_of_group 'БИ-11';
 
 -- 4)	Дать среднюю оценку студентов по каждому предмету для тех предметов, по которым занимается не менее 10 студентов.
+SELECT AVG(R.Rating) AS AvgRating, J.SubjectId, Sub.Name FROM Rating AS R
+INNER JOIN Student AS S ON S.StudentId = R.StudentId
+INNER JOIN Job AS J ON J.JobId = R.JobId
+INNER JOIN Subject AS Sub ON Sub.SubjectId = J.SubjectId
+WHERE J.SubjectId IN ( 
+  SELECT J.SubjectId FROM Job AS J
+  GROUP BY J.SubjectId
+  HAVING COUNT(J.JobId) >= 10
+)
+GROUP BY J.SubjectId, Sub.Name
 
-SELECT J.SubjectId FROM Job AS J
-GROUP BY J.SubjectId
-HAVING COUNT(J.JobId) >= 10
+-- 5)	Дать оценки студентов специальности ВМ по всем проводимым предметам с указанием группы, фамилии, предмета, даты. 
+--		При отсутствии оценки заполнить значениями NULL поля оценки и даты.
+SELECT C.Spec, C.Abbreviation, S.Surname, Sub.Name, J.JobDate, R.Rating FROM Student AS S
+INNER JOIN Class AS C ON C.ClassId = S.ClassId
+INNER JOIN Job AS J ON J.ClassId = C.ClassId
+LEFT JOIN Rating AS R ON R.JobId = J.JobId
+INNER JOIN Subject AS Sub ON Sub.SubjectId = J.SubjectId
+WHERE C.Spec = 'ВМ'
+ORDER BY C.Abbreviation, S.Surname
 
-
-
-
+-- 6)	Всем студентам специальности ИВТ, получившим оценки меньшие 5 по предмету БД до 12.05, повысить эти оценки на 1 балл.
+UPDATE Rating
+SET Rating.Rating = Rating + 1
+  FROM Rating AS R
+  INNER JOIN Job AS J ON J.JobId = R.RatingId
+  INNER JOIN Class AS C ON C.ClassId = J.ClassId
+  INNER JOIN Subject AS Sub ON Sub.SubjectId = J.SubjectId
+  WHERE J.JobDate < '2018-12-05' AND R.Rating < 5 AND C.Spec = 'ИВТ' AND Sub.Name = 'БД'
 
 
